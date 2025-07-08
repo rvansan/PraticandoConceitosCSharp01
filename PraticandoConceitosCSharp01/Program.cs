2f6a0cc0f20c7956fd68e513d2ba582c7c00989a
rvansan <vansan.rafael@gmail.com>
2025-07-08 17:45:01 -0300

    criação do menu da aplicação e do primeiro desafio, boas vindas.

--- PraticandoConceitosCSharp01/Program.cs
+++ PraticandoConceitosCSharp01/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool executar = true;
+do
+{
+    Console.WriteLine("\nPRATICANDO CONCEITOS 01");
+    ExibirOpcoes();
+    ExecutarOpcoes();
+
+} while (executar);
+
+
+
+void ExibirOpcoes()
+{
+    Console.WriteLine("\t1. Boas Vindas");
+    Console.WriteLine("\t0. Sair");
+}
+
+void ExecutarOpcoes()
+{
+    Console.Write("ESCOLHA UMA OPÇÃO: ");
+    string? opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "0":
+            executar = false;
+            break;
+        case "1":
+            BoasVindas();
+            break;
+        default:
+            Console.WriteLine("Opção Inválida!!");
+            break;
+    }
+}
+
+
+
+void BoasVindas()
+{
+    // Crie um programa em que o usuário precisa digitar um nome e uma mensagem de boas vindas
+    // personalizada com o nome dele é exibida:  Olá, Welisson! Seja muito bem-vindo!
+    Console.WriteLine("\nBOAS VINDAS");
+    Console.Write("Digite o seu nome: ");
+    string nome = Console.ReadLine();
+    string mensagem = $"Olá, {nome}! Seja muito bem-vindo!";
+    Console.WriteLine(mensagem);
+
+}
